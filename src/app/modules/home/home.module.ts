e01d9937f8974860076a75ea77c5f49c129c924c
Aidan Cole <a1dancole@icloud.com>
2021-05-05 21:26:48 +0100

    Add some external links

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { YouTubePlayerModule } from "@angular/youtube-player";
 import { SharedModule } from "../shared/shared.module";
 import { HomeComponent } from "./home.component";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, RouterModule]
+  imports: [SharedModule, RouterModule, YouTubePlayerModule]
 })
 export class HomeModule { }
