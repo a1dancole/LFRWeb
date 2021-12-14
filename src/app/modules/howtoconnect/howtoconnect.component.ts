6d009a296855fff3c7999bd81fc01faa2fc2b9c9
Aidan Cole <a1dancole@icloud.com>
2021-12-14 01:44:23 +0000

    Wiki updates and finally fix the download link on how to connect

--- src/app/modules/howtoconnect/howtoconnect.component.ts
+++ src/app/modules/howtoconnect/howtoconnect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Component({
   styleUrls: ['./howtoconnect.component.scss'],
@@ -9,5 +10,9 @@ export class HowToConnectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public downloadClient(): void {
+    window.open(environment.clientDownloadUrl, "_blank");
   }
 }
